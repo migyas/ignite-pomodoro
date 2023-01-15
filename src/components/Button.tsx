91064ed7c9f83a53aeaf94518edc847309603ed1
Yan Dias <yansantos0712@gmail.com>
2023-01-15 15:51:30 -0300

    feat: home static page completed

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, VariantColor } from "./Button.styles";
-
-interface ButtonProps {
-  variant?: VariantColor;
-}
-
-export function Button({ variant = "primary" }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Hope</ButtonContainer>;
-}
